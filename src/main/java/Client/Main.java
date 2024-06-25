cd6db73f3b3d362e8659de833b1ed00a54d8f5a2
Austin <Austin.reid@keyin.com>
2024-06-25 14:22:00 -0230

    HTTP Instance setup for Client.

--- src/main/java/Client/Main.java
+++ src/main/java/Client/Main.java
@@ -7,10 +7,14 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+            /* Insantiate Scanner for inputs*/
+
+        Scanner scanner = new Scanner(System.in);
 
         /* Here we create a new HTTP instance of HttpClient, used to request and receive responses.
          * Here we build the URI which is set to port 8080 based on address of local server.*/
@@ -34,5 +38,37 @@ public class Main {
             System.out.println("Error: " + e.getMessage());
         }
 
+        while (true){
+            System.out.println("\nAirline Management System");
+            System.out.println("1. Airport/Aircraft Menu");
+            System.out.println("2. Passenger Management");
+            System.out.println("3. City Management");
+            System.out.println("4. Exit");
+
+            System.out.println("Enter your choice: ");
+            int choice = scanner.nextInt();
+
+            switch (choice){
+                case 1:
+                    /* Austin Implement Aircraft/Airport Menu & Logic*/
+
+                case 2:
+                    /* Daniel Implement Passenger Menu & Logic*/
+
+                case 3:
+                    /* Beck Implement Passenger Menu & Logic*/
+
+                case 4:
+                    System.exit(0);
+                    break;
+
+
+                default:
+                    System.out.println("Invalid Choice");
+                    break;
+            }
+        }
     }
+
+
 }
